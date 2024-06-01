56f51b3bd7bece0f6c30d0e4a118fb12b42e2ad2
Ankur2024 <ag3942280@gmail.com>
2024-06-01 14:47:05 +0530

    Finish the Home Screen

--- app/src/main/java/com/loc/newsapp/presentation/common/ArticelList.kt
+++ app/src/main/java/com/loc/newsapp/presentation/common/ArticelList.kt
@@ -1,2 +1,76 @@
 package com.loc.newsapp.presentation.common
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.paging.LoadState
+import androidx.paging.compose.LazyPagingItems
+import com.loc.newsapp.domain.model.Article
+import com.loc.newsapp.presentation.Dimens.ExtraSmallPadding
+import com.loc.newsapp.presentation.Dimens.ExtraSmallPadding2
+import com.loc.newsapp.presentation.Dimens.MediumPadding1
+
+@Composable
+fun ArticleList(
+    modifier: Modifier = Modifier,
+    article: LazyPagingItems<Article>,
+    onClick: (Article) -> Unit
+) {
+    val handlePagingResult = HandlePagingResult(article = article)
+    if (handlePagingResult) {
+        LazyColumn(
+            modifier = Modifier.fillMaxSize(),
+            verticalArrangement = Arrangement.spacedBy(MediumPadding1),
+            contentPadding = PaddingValues(all = ExtraSmallPadding2)
+        ) {
+            items(count = article.itemCount) { index ->
+                article[index]?.let {
+                    ArticleCard(
+                        article = it,
+                        onClick = { onClick(it) }
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun HandlePagingResult(article: LazyPagingItems<Article>): Boolean {
+    val loadState = article.loadState
+    val error = when {
+        loadState.refresh is LoadState.Error -> loadState.refresh as LoadState.Error
+        loadState.prepend is LoadState.Error -> loadState.prepend as LoadState.Error
+        loadState.append is LoadState.Error -> loadState.append as LoadState.Error
+        else -> null
+    }
+    return when {
+        loadState.refresh is LoadState.Loading -> {
+            ShimmerEffect()
+            false
+        }
+        error != null -> {
+            EmptyScreen()
+            false
+        }
+        else -> true
+    }
+}
+
+@Composable
+fun ShimmerEffect() {
+    Column(
+        verticalArrangement = Arrangement.spacedBy(MediumPadding1)
+    ) {
+        repeat(10) {
+            ArticleCardShimmerEffect(
+                modifier = Modifier.padding(horizontal = MediumPadding1)
+            )
+        }
+    }
+}
